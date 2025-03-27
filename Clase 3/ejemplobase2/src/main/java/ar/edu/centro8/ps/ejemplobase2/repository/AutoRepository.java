314f34e5b98654d8cec806f66926d075653194c5
cfpit <127329179+cfpit@users.noreply.github.com>
2025-03-27 14:05:08 -0300

    Delete Clase 3/ejemplobase2 directory

--- Clase 3/ejemplobase2/src/main/java/ar/edu/centro8/ps/ejemplobase2/repository/AutoRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.centro8.ps.ejemplobase2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ar.edu.centro8.ps.ejemplobase2.model.Auto;
-
-public interface AutoRepository extends JpaRepository<Auto, Long> {
-    
-}
-
